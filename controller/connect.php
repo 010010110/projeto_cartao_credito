ab6240b05db54b0fdfe2e5d8cd8ba3350a7254bf
Carneiro <gabrielarruda08@gmail.com>
2021-03-13 14:05:42 -0300

    connect_banco

---
+++ controller/connect.php
@@ -0,0 +1,11 @@
+<?php
+    //setar valores de acordo com seu banco mysql
+    define( 'MYSQL_HOST', 'localhost' );
+    define( 'MYSQL_USER', 'root' );
+    define( 'MYSQL_PASSWORD', '' );
+    define( 'MYSQL_DB_NAME', '' ); //inserir nome do banco
+    try{
+        $connection = new PDO( 'mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB_NAME, MYSQL_USER, MYSQL_PASSWORD );
+    }catch ( PDOException $e ){
+        echo 'Erro ao conectar com o MySQL: ' . $e->getMessage();
+    }
