8711a8a5677b17ad4a2677893ad9966348ae4d23
Carneiro <gabrielarruda08@gmail.com>
2021-03-14 18:13:20 -0300

    verifica_situacao_cadastro

--- model/situacao.php
+++ model/situacao.php
@@ -3,20 +3,22 @@
 
     $cpf = $_POST['cpf'];
     
-    $stmt = $pdo->prepare( 'SELECT situacao FROM user WHERE cpf = $cpf');
-    //$stmt->bindValue( ':cpf', $cpf );
-    // $situacao = $stmt->execute();
+    $sql = 'SELECT situacao FROM user WHERE cpf = :cpf LIMIT 1';
+    
+    $sth = $pdo->prepare($sql);
+    $sth->execute(array(':cpf' => $cpf));
+    $result = $sth->fetch();
 
-    // if (!$situacao){
-    //     echo  "<script>alert('Cadastro Nao Encontrado!');
-    //                 window.location='http://localhost/Code/login/view/login.php';
-    //             </script>";
-    // }else if($situacao == 1){
-    //     echo  "<script>alert('Cadastro Aprovado! faca login para saber mais');
-    //                 window.location='http://localhost/Code/login/view/login.php';
-    //             </script>";
-    // }else {
-    //     echo  "<script>alert('Cadastro nao Aprovado!');
-    //                 window.location='http://localhost/Code/login/view/login.php';
-    //             </script>";
-    // }
+    if (!$result){
+        echo  "<script>alert('Cadastro Nao Encontrado!');
+                    window.location='http://localhost/Code/cartao_credito/view/login.php';
+                </script>";
+    }else if($result['situacao'] == 1){
+        echo  "<script>alert('Cadastro Aprovado! faca login para saber mais');
+                    window.location='http://localhost/Code/cartao_credito/view/login.php';
+                </script>";
+    }else {
+        echo  "<script>alert('Cadastro nao Aprovado!');
+                    window.location='http://localhost/Code/cartao_credito/view/login.php';
+                </script>";
+    }
