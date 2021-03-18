6997898b14b40827de48a3cce79c1b62b737545e
Carneiro <gabrielarruda08@gmail.com>
2021-03-18 16:25:52 -0300

    update_end_user

---
+++ model/update_endereco.php
@@ -0,0 +1,26 @@
+<?php
+    require_once('../controller/connect.php');
+
+    session_start();
+
+    $nome = $_SESSION['user'];
+    $logradouro = $_POST['logradouro'];
+    $numero = $_POST['num'];
+    $bairro = $_POST['bairro'];
+    $cidade = $_POST['cidade'];
+    $uf = $_POST['uf'];
+    $pais = $_POST['pais'];
+
+    $stmt = $pdo->prepare('UPDATE user SET logradouro=:logradouro, numero=:numero, bairro=:bairro, cidade=:cidade, uf=:uf, pais=:pais WHERE nome = :nome');
+    $stmt->execute(array(
+    ':nome'   => $nome,
+    ':logradouro' => $logradouro,
+    ':numero' => $numero,
+    ':bairro' => $bairro,
+    ':cidade' => $cidade,
+    ':uf' => $uf,
+    ':pais' => $pais
+    ));
+    echo    "<script>alert('Atualizacao Realizada!');
+                location.href='../view/main.php';
+            </script>";
---
+++ view/update_endereco.php
@@ -0,0 +1,38 @@
+<?php
+    session_start();
+    if(empty($_SESSION['logado']) || $_SESSION['logado'] == false){
+        header('Location: login.php');
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+    <title>Update Endereco</title>
+</head>
+<body>
+    <header>
+        <h1>Atualizar Endereco:</h1>
+        <p>Atualize seu Endereco</p>
+    </header>
+    <main>
+        <form method="POST" action="../model/update_endereco.php">
+            <fieldset>
+                <input type="text" name="logradouro" id="logradouro" placeholder="Rua">
+                <input type="number" name="num" id="num" placeholder="Numero">
+                <input type="text" name="bairro" id="bairro" placeholder="Bairro">
+                <input type="text" name="cidade" id="cidade" placeholder="Cidade">
+                <input type="text" name="uf" id="uf" placeholder="UF" maxlength="2">
+                <input type="text" name="pais" id="pais" placeholder="Pais">
+            </fieldset>
+            <button>Atualizar</button>
+        </form>
+    </main>
+    <footer>
+      Empresa de Cartao de credito
+    </footer>
+</body>
