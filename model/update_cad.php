6e18265db50b2ffe07a3f151914d7ca8c288c15a
Carneiro <gabrielarruda08@gmail.com>
2021-03-18 16:25:29 -0300

    update_cad_user

---
+++ model/update_cad.php
@@ -0,0 +1,20 @@
+<?php
+    require_once('../controller/connect.php');
+
+    session_start();
+
+    $nome = $_SESSION['user'];
+    $tel = $_POST['tel'];
+    $renda = $_POST['renda'];
+    $senha = md5($_POST['password']);
+
+    $stmt = $pdo->prepare('UPDATE user SET telefone=:tel, renda_mensal=:renda, senha=:senha WHERE nome = :nome');
+    $stmt->execute(array(
+    ':nome'   => $nome,
+    ':tel' => $tel,
+    ':renda' => $renda,
+    'senha' => $senha
+    ));
+    echo    "<script>alert('Atualizacao Realizada!');
+                location.href='../view/main.php';
+            </script>";
