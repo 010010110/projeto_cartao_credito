e0f438bc9d5c0577433956a3f82483e209704de9
Carneiro <gabrielarruda08@gmail.com>
2021-03-18 19:55:56 -0300

    create: Class to connect bd

--- controller/connect.php
+++ controller/connect.php
@@ -4,8 +4,22 @@
     define( 'MYSQL_USER', 'root' );
     define( 'MYSQL_PASSWORD', '' );
     define( 'MYSQL_DB_NAME', 'project_1' ); //inserir nome do banco
-    try{
-        $pdo = new PDO( 'mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB_NAME, MYSQL_USER, MYSQL_PASSWORD );
-    }catch ( PDOException $e ){
-        echo 'Erro ao conectar com o MySQL: ' . $e->getMessage();
+    // try{
+    //     $pdo = new PDO( 'mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB_NAME, MYSQL_USER, MYSQL_PASSWORD );
+    // }catch ( PDOException $e ){
+    //     echo 'Erro ao conectar com o MariaDB: ' . $e->getMessage();
+    // }
+
+    class Conexao {
+        private static $pdo;
+    
+        public static function get() {
+            try {
+                if(!isset(self::$pdo))
+                    self::$pdo = new PDO('mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB_NAME, MYSQL_USER, MYSQL_PASSWORD);
+                return self::$pdo;
+            } catch(Exception $e) {
+                throw new Exception($e->getMessage());
+            }
+        }
     }
