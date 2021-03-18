8ee8e12583789f07833d540df1cfe7cee14f1666
Carneiro <gabrielarruda08@gmail.com>
2021-03-18 16:26:34 -0300

    create_funcionario_login

---
+++ model/funcionario/login.php
@@ -0,0 +1,31 @@
+<?php
+
+    require_once('../../controller/connect.php');
+
+    //pegras credenciais
+    $func = $_POST['funcionario'] ?? '';
+    $senha = $_POST['password'] ?? '';
+
+    //inicialzia session
+    session_start();
+        
+    $sth = $pdo->prepare('SELECT * FROM funcionario WHERE (func_login=:func AND func_senha=:senha)');
+    $result = $sth->execute(array(':func' => $func, ':senha' => $senha));
+    $result = $sth->fetch(PDO::FETCH_OBJ);
+    //var_dump($result) ;
+    //checar credenciais OK
+    if($result == true){
+        $_SESSION['login_func'] = true;
+        $_SESSION['func'] = $_POST['func'];
+        header('Location: ../../view/funcionario/main.php');
+
+    }else if($result == false){
+        echo  "<script>alert('Login Invalido!');
+                    location.href='../../view/funcionario/login.php';
+               </script>";
+    }
+
+    //checar se user ja ta logado
+    if(!empty($_SESSION['login_func']) && $_SESSION['login_func']){
+        header('Location: ../../view/funcionario/main.php');
+    }
