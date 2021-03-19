f21ea7d8c9e5f15ec63e74569d60636e0e284e42
listanegra <vitormac3@hotmail.com>
2021-03-19 15:15:32 -0300

    Refatoração tela de login

--- view/login.php
+++ view/login.php
@@ -4,26 +4,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
-    <title>login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Entrar</title>
 </head>
-<body>
-<header>
-    <h1> Login:</h1>
-</header>
-<main>
-    <form method="POST" action="../model/login.php">
-        <!-- <?php if ($err) : ?>
-              <div>Usuario ou Senha invalidos</div>
-            <?php endif; ?> -->
-        <input type="text" name="user" id="user" placeholder="User Name" required>
-        <input type="password" name="password" id="password" placeholder="Password" required><br>
-        <button>Login</button>
-    </form>
-</main>
-<footer>
-    Empresa de Cartao de credito
-</footer>
+<body class="vh-100">
+<div class="p-2 h-100">
+    <div class="row align-items-center justify-content-center h-100">
+        <div class="col-4 m-auto p-0 card text-center">
+            <div class="card-header">
+                Login
+            </div>
+            <div class="card-body">
+                <form method="POST" action="../model/login.php">
+                    <h5 class="card-title">Entrar na sua conta</h5>
+                    <div class="mb-3 row">
+                        <label for="user" class="col-sm-2 col-form-label">Usuário:</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="user" id="user">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="password" class="col-sm-2 col-form-label">Senha:</label>
+                        <div class="col-sm-10">
+                            <input type="password" class="form-control" name="password" id="password">
+                        </div>
+                    </div>
+                    <input type="submit" class="btn btn-primary" value="Entrar">
+                </form>
+            </div>
+            <div class="card-footer text-muted">
+                Empresa de Cartão de Crédito
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
