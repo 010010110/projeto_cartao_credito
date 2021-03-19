a08eb87a39bdfb51a7bc303a5702d3862e842a03
listanegra <vitormac3@hotmail.com>
2021-03-19 15:49:34 -0300

    Refatoração tela principal

--- view/main.php
+++ view/main.php
@@ -11,23 +11,40 @@ if (empty($_SESSION['logado']) || $_SESSION['logado'] == false) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
-    <title>login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Home</title>
 </head>
-<body>
+<body class="vh-100">
+
 <header>
-    <h1>Empresa</h1>
-    <p>Olá <?= $_SESSION['user'] ?></p>
-    <nav>
-        <a href="../model/logout.php">Sair</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Empresa</a>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                </ul>
+            </div>
+            <a class="btn btn-primary" href="../model/logout.php">Sair</a>
+        </div>
     </nav>
 </header>
-<main>
+
+<main class="container py-2">
     <h2>Bem vindo!</h2>
+
+    <p>Olá <?= $_SESSION['user'] ?></p>
 </main>
-<footer>
-    Empresa de Cartão de crédito
+
+<footer class="position-absolute bottom-0 w-100 bg-light">
+    <div class="container my-4">
+        <div class="text-center">Empresa de Cartão de crédito</div>
+    </div>
 </footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
