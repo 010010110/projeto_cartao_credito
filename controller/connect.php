8e611c112c754627bf2585158d94084a25e5bdf7
listanegra <vitormac3@hotmail.com>
2021-04-04 22:43:33 -0300

    Ajuste banco

--- controller/connect.php
+++ controller/connect.php
@@ -1,25 +1,8 @@
 <?php
-    //setar valores de acordo com seu banco mysql
-    define( 'MYSQL_HOST', 'localhost' );
-    define( 'MYSQL_USER', 'root' );
-    define( 'MYSQL_PASSWORD', '' );
-    define( 'MYSQL_DB_NAME', 'project_1' ); //inserir nome do banco
-    // try{
-    //     $pdo = new PDO( 'mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB_NAME, MYSQL_USER, MYSQL_PASSWORD );
-    // }catch ( PDOException $e ){
-    //     echo 'Erro ao conectar com o MariaDB: ' . $e->getMessage();
-    // }
 
-    class Conexao {
-        private static $pdo;
-    
-        public static function get() {
-            try {
-                if(!isset(self::$pdo))
-                    self::$pdo = new PDO('mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB_NAME, MYSQL_USER, MYSQL_PASSWORD);
-                return self::$pdo;
-            } catch(Exception $e) {
-                throw new Exception($e->getMessage());
-            }
-        }
-    }
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_USER', 'root');
+define('MYSQL_PASSWORD', '');
+define('MYSQL_DB_NAME', 'empresinha');
+
+$pdo = new PDO('mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB_NAME, MYSQL_USER, MYSQL_PASSWORD);
