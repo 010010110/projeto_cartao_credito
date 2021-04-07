14e80c10c22256b74607c8132566cef36da610b9
Gabriel Carneiro <gabrielarruda08@gmail.com>
2021-04-07 20:09:01 -0300

    result select to json

--- model/pedidos_cadastro.php
+++ model/pedidos_cadastro.php
@@ -4,6 +4,7 @@ if (empty($_SESSION['login']) || $_SESSION['login'] == false) {
     header('Location: login.php');
 }
 
+require_once('../utils/utils.php');
 require_once('../controller/connect.php');
 
 global $pdo;
@@ -14,4 +15,8 @@ $stmt = $pdo->prepare("SELECT u.*, p.*, e.* FROM user u
     INNER JOIN pessoa p ON p.id = u.pessoa_id
     INNER JOIN endereco e ON e.id = p.endereco_id 
     WHERE u.status = 'I'");
-$result->execute();
+$stmt->execute();
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+Utils::json($results);
