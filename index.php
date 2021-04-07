23c2fdb61c345ab476837c47bfbf1cbac83cdbdc
Gabriel Carneiro <gabrielarruda08@gmail.com>
2021-04-07 19:49:16 -0300

    session rename

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
-if (empty($_SESSION['logado']) || $_SESSION['logado'] == false) {
+if (empty($_SESSION['login']) || $_SESSION['login'] == false) {
     header('Location: view/index.php');
 } else header('Location: view/main.php');
