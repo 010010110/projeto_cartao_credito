cf7ee2a7d449405eb4a1638d895af845975b99f2
listanegra <vitormac3@hotmail.com>
2021-04-07 21:23:31 -0300

    Adicionado service para API

---
+++ src/services/api-service.ts
@@ -0,0 +1,21 @@
+import Axios from 'axios'
+
+const axios = Axios.create({
+    baseURL: process.env['VUE_APP_API_ENDPOINT']
+});
+
+axios.interceptors.response.use(response => {
+    if (response.data['error']) {
+        throw new Error(response.data['message']);
+    }
+
+    return response;
+});
+
+export class ApiService {
+
+    public static login(email: string, senha: string) {
+        return axios.post('/model/login.php', { email, senha });
+    }
+
+}
