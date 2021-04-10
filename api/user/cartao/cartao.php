db1100648efb9427d4e34a077b2c73162b81fc22
listanegra <vitormac3@hotmail.com>
2021-04-10 16:29:44 -0300

    Rename endpoints

--- api/user/cartao/cartao.php
+++ api/user/cartao/cartao.php
@@ -1,10 +1,10 @@
 <?php
-require_once('../../utils/connect.php');
+require_once('../../../utils/connect.php');
 
 global $pdo;
 
-require_once('../../utils/session_util.php');
-require_once('../../utils/utils.php');
+require_once('../../../utils/session_util.php');
+require_once('../../../utils/utils.php');
 Utils::cors();
 
 $data = file_get_contents('php://input');
@@ -22,19 +22,17 @@ if (!array_key_exists('status', $payload)) {
     exit();
 }
 
-$email = $_SESSION['user'];
+$user_id = $_SESSION['id'];
 
 $cartao_id = $payload['cartao_id'];
 $status = $payload['status'];
 
-$stmt = $pdo->prepare('SELECT id FROM user WHERE email = :email');
-$stmt->bindParam(':email', $email);
-$stmt->execute();
+$stmt = $pdo->prepare('
+SELECT * FROM user_has_cartao 
+WHERE user_id = :user_id AND cartao_id = :cartao_id
+');
 
-$user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-$stmt = $pdo->prepare('SELECT * FROM user_has_cartao WHERE user_id = :user_id AND cartao_id = :cartao_id');
-$stmt->bindParam(':user_id', $user['id']);
+$stmt->bindParam(':user_id', $user_id);
 $stmt->bindParam(':cartao_id', $cartao_id);
 $stmt->execute();
 
@@ -44,7 +42,11 @@ if (!$stmt->rowCount()) {
     exit();
 }
 
-$stmt = $pdo->prepare('UPDATE cartao SET status = :status WHERE id = :cartao_id');
+$stmt = $pdo->prepare('
+UPDATE cartao SET status = :status
+WHERE id = :cartao_id
+');
+
 $stmt->bindParam(':status', $status);
 $stmt->bindParam(':cartao_id', $cartao_id);
 $stmt->execute();
