380b0f74d770cedbf41394304f5d8dbeadec48a7
listanegra <vitormac3@hotmail.com>
2021-04-10 17:18:58 -0300

    Layout básico faturas

--- src/views/home/Faturas.vue
+++ src/views/home/Faturas.vue
@@ -1,1 +1,16 @@
-<template></template>
+<template>
+    <v-row class="fill-height">
+        <v-col class="fill-height py-0" cols="6"></v-col>
+        <v-col class="fill-height py-0" cols="6"></v-col>
+    </v-row>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class Faturas extends Vue {
+
+}
+</script>
