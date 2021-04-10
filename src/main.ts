28046c039956f2a65064c56e4688fc131b9104e5
listanegra <vitormac3@hotmail.com>
2021-04-09 23:53:03 -0300

    Adicionado lib vue-cookies

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify';
+
+import '@/plugins/vue-cookies'
+
+import router from '@/router'
+import vuetify from '@/plugins/vuetify'
 
 Vue.config.productionTip = false
 
