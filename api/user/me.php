48dbbf084e60147e22204c234fcc17d73f441513
listanegra <vitormac3@hotmail.com>
2021-04-11 13:44:25 -0300

    Não retornar todas as infos do user na API

--- api/user/me.php
+++ api/user/me.php
@@ -10,7 +10,10 @@ Utils::cors();
 $email = $_SESSION['user'];
 
 $st = $pdo->prepare("
-SELECT u.*, p.*, e.* FROM user u
+SELECT u.tipo as tipo_usuario, u.email, u.renda_mensal, u.limite,
+       p.nome, p.documento, p.telefone, p.tipo as tipo_pessoa,
+       e.cep, e.numero
+FROM user u
     INNER JOIN pessoa p ON u.pessoa_id = p.id
     INNER JOIN endereco e ON p.endereco_id = e.id
 WHERE u.email = :email
