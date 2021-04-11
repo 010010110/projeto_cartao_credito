251a7c79215e1f16dc6dceabba7925dd1a92dc8f
listanegra <vitormac3@hotmail.com>
2021-04-11 13:58:20 -0300

    Tela para exibição dos cartões do usuário

---
+++ src/components/Cartao.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="cartao primary rounded-lg">
+        <div class="overlay" />
+        <div class="content d-flex flex-column justify-space-between pa-4">
+            <span class="headline font-weight-bold white--text text-center" v-text="numero" />
+            <div class="white--text">
+                <div class="d-flex justify-space-between">
+                    <span v-text="data" />
+                    <span v-text="validade" />
+                </div>
+                <span class="subtitle-1 text-uppercase" v-text="titular" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class Cartao extends Vue {
+
+    @Prop({ required: true })
+    private numero!: string;
+
+    @Prop({ required: true })
+    private data!: string;
+
+    @Prop({ required: true })
+    private validade!: string;
+
+    @Prop({ required: true })
+    private titular!: string;
+
+}
+</script>
+
+<style lang="scss" scoped>
+div.cartao {
+    width: 320px;
+    height: 200px;
+    position: relative;
+    user-select: none;
+
+    background-size: cover;
+    background-position: center;
+    background-image: url("https://image.freepik.com/vetores-gratis/wallpaper-de-paisagem-em-design-plano_74890-34.jpg");
+
+    > div {
+        width: 100%;
+        height: 100%;
+        border-radius: inherit;
+        position: absolute;
+    }
+
+    > div.overlay {
+        backdrop-filter: blur(10px);
+        background-color: rgba($color: #fff, $alpha: 0.1);
+    }
+
+    > div.content {
+        z-index: 1;
+        position: absolute;
+    }
+}
+</style>
--- src/views/home/Cartoes.vue
+++ src/views/home/Cartoes.vue
@@ -1,1 +1,45 @@
-<template></template>
+<template>
+    <div class="fill-height" style="width: 100%">
+        <div class="display-1 font-weight-bold text-center pa-4">Seus cartões</div>
+        <v-row v-if="cartoes.length">
+            <v-col v-for="(cartao, i) in cartoes" :key="i" class="pa-2" cols="6">
+                <v-card>
+                    <v-card-text class="d-inline-flex">
+                        <CartaoComponent
+                            :numero="cartao.numero"
+                            :data="cartao.data_emissao"
+                            :validade="cartao.validade"
+                            :titular="cartao.titular"
+                        ></CartaoComponent>
+                        <v-col class="pa-0">
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+
+                                <v-btn color="red" icon>
+                                    <v-icon>mdi-close</v-icon>
+                                </v-btn>
+                            </v-card-actions>
+                        </v-col>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+        <div class="subtitle-1 grey--text text-center" v-else>Não há cartões registrados ainda :(</div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+import CartaoComponent from '@/components/Cartao.vue'
+
+import { Cartao } from '@/services/api-service'
+
+@Component({ components: { CartaoComponent } })
+export default class Cartoes extends Vue {
+
+    private cartoes: Cartao[] = [];
+
+}
+</script>
