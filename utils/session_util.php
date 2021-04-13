4528062c454030489ad4a5e1332a1459d858708d
listanegra <vitormac3@hotmail.com>
2021-04-12 22:49:30 -0300

    Ajuste session PHP

--- utils/session_util.php
+++ utils/session_util.php
@@ -8,5 +8,11 @@ if (empty($_SESSION['login']) || !$_SESSION['login']) {
     Utils::json(['message' => "Usuário não autenticado", 'error' => true]);
     http_response_code(403);
 
+    session_unset();
+    session_abort();
+    session_write_close();
+
+    header_remove('Set-Cookie');
+
     exit();
 }
