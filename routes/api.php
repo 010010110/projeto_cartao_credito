4b166b337014d039c814ff0ca74d4d111118813a
listanegra <vitormac3@hotmail.com>
2021-05-14 23:30:16 -0300

    Rotas dos controllers

--- routes/api.php
+++ routes/api.php
@@ -15,15 +15,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/register', 'App\Http\Controllers\AuthController@register');
-
 Route::post('/login', 'App\Http\Controllers\AuthController@login');
 Route::post('/logout', 'App\Http\Controllers\AuthController@logout');
 
+Route::post('/cadastro', 'App\Http\Controllers\AuthController@register');
+
 Route::group(['middleware' => ['api', 'auth']], function () {
-    Route::get('/user', function (Request $request) {
-        $user = Auth::user();
-        return $user;
+    Route::group(['prefix' => '/user'], function () {
+        Route::get('/', 'App\Http\Controllers\UserController@getUser');
+
+        Route::group(['prefix' => '/cartoes'], function () {
+            Route::get('/', 'App\Http\Controllers\CartaoController@getCartoes');
+            Route::get('/bandeiras', 'App\Http\Controllers\CartaoController@getBandeiras');
+            Route::post('/cadastro', 'App\Http\Controllers\CartaoController@criar');
+        });
+
+        Route::group(['prefix' => '/faturas'], function () {
+            Route::get('/', 'App\Http\Controllers\FaturaController@getFaturas');
+            Route::post('/', 'App\Http\Controllers\FaturaController@simular');
+            Route::put('/pagar', 'App\Http\Controllers\FaturaController@pagar');
+        });
+    });
+
+    Route::group(['prefix' => 'admin'], function () {
+        Route::get('/funcionarios', 'App\Http\Controllers\UserController@getFuncionarios');
+    });
+
+    Route::group(['prefix' => 'funcionario'], function () {
+        Route::get('/usuarios', 'App\Http\Controllers\UserController@getUsers');
+        Route::get('/pedidos', 'App\Http\Controllers\CartaoController@getPendentes');
+        Route::put('/cartao', 'App\Http\Controllers\CartaoController@update');
+        Route::put('/user', 'App\Http\Controllers\UserController@update');
     });
 });
 
